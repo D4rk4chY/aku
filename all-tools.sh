4692a73eeda732131a677e02132e0269bcb4d4be
D4rk4chY <53229584+D4rk4chY@users.noreply.github.com>
2019-08-03 08:24:25 +0700

    Add files via upload

---
+++ all-tools.sh
@@ -0,0 +1,213 @@
+#user/bin/bash
+#/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+#\/                                                                            \/
+#/\                         No System Is Safe                                  /\
+#\/                             Attacker                                       \/
+#/\                              HACKED                                        /\
+#\/                             Phishing                                       \/
+#/\                                                                            /\
+#\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+#PERINGATAN : KESALAHAN ATAU KEJAHATAN ADALAH TANGGUNG JAWAB DARI PEMAKAI
+#GUNAKAN TOOLS UNTUK KEBAIKAN JANGAN DISALAH GUNAKAN :)
+#ini warna
+green="\033[32;1m"
+yellow="\033[33;1m"
+bluesky="\033[34;1mBlink" 
+magenta="\033[35;1m"
+red="\3[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+clear
+echo 
+echo 
+clear
+echo 
+echo -e $bluesky"                  ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶"
+sleep 0.1
+echo -e $bluesky"              ¶¶¶¶¶¶             ¶¶¶¶¶¶¶"
+sleep 0.1
+echo -e $bluesky"           ¶¶¶¶                        ¶¶¶¶"
+sleep 0.1
+echo -e $bluesky"          ¶¶¶                            ¶¶¶"
+sleep 0.1
+echo -e $bluesky"         ¶¶      No System Is Safe         ¶¶"
+sleep 0.1
+echo -e $bluesky"        ¶¶            Attacker             ¶¶"
+sleep 0.1
+echo -e $bluesky"       ¶¶              HACKED               ¶¶"
+sleep 0.1
+echo -e $bluesky"       ¶¶´¶¶          Phishing           ¶¶´¶¶"
+sleep 0.1
+echo -e $bluesky"       ¶¶´¶¶                             ¶¶´´¶"
+sleep 0.1
+echo -e $bluesky"       ¶¶´¶¶                             ¶¶´´¶"
+sleep 0.1
+echo -e $bluesky"      ¶¶  ¶¶                            ¶¶´¶¶"
+sleep 0.1
+echo -e $bluesky"       ¶¶´´¶¶                           ¶¶´´¶¶"
+sleep 0.1
+echo -e $bluesky"        ¶¶´¶¶   ¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶   ¶¶´¶¶"
+sleep 0.1
+echo -e $bluesky"         ¶¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶¶"
+sleep 0.1
+echo -e $bluesky"          ¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶"
+sleep 0.1
+echo -e $bluesky"  ¶¶¶      ¶¶  ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶¶  ¶¶      ¶¶¶¶"
+sleep 0.1
+echo -e $bluesky" ¶¶¶¶¶    ¶¶   ¶¶¶¶¶¶¶   ¶¶¶   ¶¶¶¶¶¶¶   ¶¶    ¶¶¶¶¶¶"
+sleep 0.1
+echo -e $bluesky"¶¶  ¶¶    ¶¶     ¶¶¶    ¶¶¶¶¶    ¶¶¶     ¶¶    ¶¶  ¶¶"
+sleep 0.1
+echo -e $bluesky"¶¶   ¶¶¶¶  ¶¶          ¶¶¶¶¶¶¶          ¶¶  ¶¶¶¶   ¶¶¶"
+sleep 0.1
+echo -e $bluesky"¶       ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶        ¶¶¶¶¶¶¶¶¶      ¶¶"
+sleep 0.1
+echo -e $bluesky"¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶"
+sleep 0.1
+echo -e $bluesky"¶¶¶¶ ¶¶¶¶¶     ¶¶¶¶¶  -D4rk4chy-  ¶¶¶ ¶¶    ¶¶¶¶¶¶ ¶¶¶"
+sleep 0.1
+echo -e $bluesky"      ¶¶¶¶¶¶  ¶¶¶  ¶¶           ¶¶  ¶¶¶  ¶¶¶¶¶¶"        
+sleep 0.1
+echo -e $bluesky"           ¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶"
+sleep 0.1
+echo -e $bluesky"                ¶¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶¶"
+sleep 0.1
+echo -e $bluesky"              ¶¶¶¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶  ¶¶¶¶¶"
+sleep 0.1
+echo -e $bluesky"          ¶¶¶¶¶ ¶¶  ¶¶¶¶¶¶¶¶¶¶¶¶¶  ¶¶ ¶¶¶¶¶"
+sleep 0.1
+echo -e $bluesky"  ¶¶¶¶¶¶¶¶¶¶    ¶¶                 ¶¶     ¶¶¶¶¶¶¶¶¶"
+sleep 0.1
+echo -e $bluesky" ¶¶          ¶¶¶¶¶¶¶             ¶¶¶¶¶¶¶¶         ¶¶"
+sleep 0.1
+echo -e $bluesky"  ¶¶¶    ¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶    ¶¶¶"
+sleep 0.1
+echo -e $bluesky"    ¶¶  ¶¶¶           ¶¶¶¶¶¶¶¶¶           ¶¶¶  ¶¶"
+sleep 0.1
+echo -e $bluesky"    ¶¶ ¶¶                                   ¶¶ ¶¶"
+sleep 0.1
+echo -e $bluesky"     ¶¶¶                                     ¶¶¶"
+sleep 0.1
+echo  
+echo -e $yellow"  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e $green"  AUTHOR   : -D4rk4chy-"
+echo -e $green"-------------------------------"
+echo -e $green"  YOUTUBE  : -Dek Achy Channel-"
+echo -e $green"-------------------------------"
+echo -e $green"  TOOLS    : ALL TOOLS"
+echo -e $yellow"  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo  "      PERINGATAN : KESALAHAN ATAU KEJAHATAN ADALAH TANGGUNG JAWAB DARI PEMAKAI"
+echo  "                   GUNAKAN TOOLS UNTUK KEBAIKAN JANGAN DISALAH GUNAKAN :)					"
+echo 
+echo 
+echo  -e $red"=========================================="
+echo  -e $red"=      -e $cyan" TOOLS HIBURAN"                  -e $red="
+echo  -e $red"=      -e $cyan" -D4rk4chy-   "                  -e $red="
+echo  -e $red"=========================================="
+echo  
+echo  -e $yellow"[1]" " Brute Force Facebook"
+echo  -e $green"=========================================="
+echo  -e $yellow"[2]" " SPAM KOMUNIKASI"
+echo  -e $green"=========================================="
+echo  -e $yellow"[7]" " Sadap Camera"
+echo 
+echo  -e $red"=========================================="
+echo  -e $red"=      -e $cyan" TOOLS NETWORK"                   -e $red="
+echo  -e $red"=      -e $cyan" -D4rk4chy-   "                   -e $red="
+echo  -e $red"=========================================="
+echo  
+echo
+echo  -e $yellow"[01]" " INSTALL MIKHMON"
+
+
+
+echo  -e $green"[exit]" " exit"
+echo " =================================================="
+echo 
+echo 
+echo   "┌===" "[ -D4rk4chy- ]"
+echo   "¦"Pilih nomer toolsnya
+read -p">>>" pil;
+
+if [ $pil = 1 ]
+then
+clear
+pkg install php -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+git clone https://github.com/D4rk4chy/spam-komunikasi
+cd spam-komunikasi
+bash spam-komunikasi.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+git clone https://github.com/zlucifer/skull_project
+cd  skull_project
+bash skull.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+git clone https://github.com/zlucifer/termux_alice
+cd termux_alice
+sh alice.sh
+fi
+
+if [ $pil = 5 ]
+then
+clear
+git clone https://github.com/zlucifer/paladin_project
+cd paladin_project
+sh paladin.sh
+fi
+
+if [ $pil = 6 ]
+then
+clear
+git clone https://github.com/zlucifer/trap_project
+cd trap_project
+bash trap.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+fi
+
+if [ $pil = 01 ]
+then
+clear
+cd
+cd
+curl -o install-mikhmon https://laksa19.github.io/install-mikhmon.txt
+chmod +x install-mikhmon
+./install-mikhmon
+./start-mikhmon
+fi
+
+if [ $pil = exit ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo   "Terima Kasih "
+sleep 2
+echo   "Salam Hangat dari"
+sleep 2
+echo   "-D4rk4chy-"
+exit
+fi
